b7b8015ffb9a14b193ba84c812c7a3f82910ffcf
Vaishnavi2028 <vaxtra157@gmail.com>
2024-11-06 16:27:59 +0000

    typeConversion

---
+++ .devcontainer/01_basics/dtyes.js
@@ -0,0 +1,8 @@
+let a = 18
+let xyz = "abc"
+let b = "18"
+let c = true
+let d = null 
+let e
+
+console.log( typeof Number(b))
--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,10 @@
-console.log("helllo world")
+console.log("hello world")
+const roll_no = 157
+let email ="vamzn2028@gmail.com"
+var name_ = "Vaishnavi"
+company = "Amazon"
+email="vaxtra2028@gmail.com"
+name_ = "Vamzn"
+company = "Amazon_Hyderabad"
+console.log( company)
+console.table([roll_no,name_ , email,company])
